ca561de3073b3302f6afab57e4ee7f6f20246fc9
yohannesalex <yohannesalemayehu624@gmail.com>
2025-02-05 00:13:15 +0300

    update the backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -116,4 +116,6 @@ def refine():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
